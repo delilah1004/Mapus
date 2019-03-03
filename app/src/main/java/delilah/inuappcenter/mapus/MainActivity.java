5696e54008b86a094dd5879fc439e2ea4dab8fdd
Delilah <pskdy8414@gmail.com>
2019-03-03 18:26:25 +0900

    custom marker

--- app/src/main/java/delilah/inuappcenter/mapus/MainActivity.java
+++ app/src/main/java/delilah/inuappcenter/mapus/MainActivity.java
@@ -90,242 +90,272 @@ public class MainActivity extends AppCompatActivity {
         // 마커 이름
         markerOne.setItemName("1 대학본부");
         markerOne.setTag(1);
-        // 기본으로 제공하는 BluePin 마커 모양.
-        markerOne.setMarkerType(MapPOIItem.MarkerType.BluePin);
-        // 마커를 클릭했을때, 기본으로 제공하는 RedPin 마커 모양.
-        markerOne.setSelectedMarkerType(MapPOIItem.MarkerType.RedPin);
+        // 커스텀 마커 모양.
+        markerOne.setMarkerType(MapPOIItem.MarkerType.CustomImage);
+        // 마커 이미지.
+        markerOne.setCustomImageResourceId(R.drawable.ic_marker);
+        markerOne.setCustomImageAutoscale(false);
         mapView.addPOIItem(markerOne);
 
         MapPOIItem markerTwo = new MapPOIItem();
         markerTwo.setMapPoint(MapPoint.mapPointWithGeoCoord(37.377443, 126.633858));
         markerTwo.setItemName("2 교수회관");
         markerTwo.setTag(2);
-        markerTwo.setMarkerType(MapPOIItem.MarkerType.BluePin);
-        markerTwo.setSelectedMarkerType(MapPOIItem.MarkerType.RedPin);
+        markerTwo.setMarkerType(MapPOIItem.MarkerType.CustomImage);
+        markerTwo.setCustomImageResourceId(R.drawable.ic_marker);
+        markerTwo.setCustomImageAutoscale(false);
         mapView.addPOIItem(markerTwo);
 
         MapPOIItem markerThree = new MapPOIItem();
         markerThree.setMapPoint(MapPoint.mapPointWithGeoCoord(37.377148, 126.634203));
         markerThree.setItemName("3 홍보관");
         markerThree.setTag(3);
-        markerThree.setMarkerType(MapPOIItem.MarkerType.BluePin);
-        markerThree.setSelectedMarkerType(MapPOIItem.MarkerType.RedPin);
+        markerThree.setMarkerType(MapPOIItem.MarkerType.CustomImage);
+        markerThree.setCustomImageResourceId(R.drawable.ic_marker);
+        markerThree.setCustomImageAutoscale(false);
         mapView.addPOIItem(markerThree);
 
         MapPOIItem markerFour = new MapPOIItem();
         markerFour.setMapPoint(MapPoint.mapPointWithGeoCoord(37.376390, 126.635593));
         markerFour.setItemName("4 BM 컨텐츠관");
         markerFour.setTag(4);
-        markerFour.setMarkerType(MapPOIItem.MarkerType.BluePin);
-        markerFour.setSelectedMarkerType(MapPOIItem.MarkerType.RedPin);
+        markerFour.setMarkerType(MapPOIItem.MarkerType.CustomImage);
+        markerFour.setCustomImageResourceId(R.drawable.ic_marker);
+        markerFour.setCustomImageAutoscale(false);
         mapView.addPOIItem(markerFour);
 
         MapPOIItem markerFive = new MapPOIItem();
         markerFive.setMapPoint(MapPoint.mapPointWithGeoCoord(37.375580, 126.634649));
         markerFive.setItemName("5 자연과학/생명과학기술대학");
         markerFive.setTag(5);
-        markerFive.setMarkerType(MapPOIItem.MarkerType.BluePin);
-        markerFive.setSelectedMarkerType(MapPOIItem.MarkerType.RedPin);
+        markerFive.setMarkerType(MapPOIItem.MarkerType.CustomImage);
+        markerFive.setCustomImageResourceId(R.drawable.ic_marker);
+        markerFive.setCustomImageAutoscale(false);
         mapView.addPOIItem(markerFive);
 
         MapPOIItem markerSix = new MapPOIItem();
         markerSix.setMapPoint(MapPoint.mapPointWithGeoCoord(37.375022, 126.634102));
         markerSix.setItemName("6 학산도서관");
         markerSix.setTag(6);
-        markerSix.setMarkerType(MapPOIItem.MarkerType.BluePin);
-        markerSix.setSelectedMarkerType(MapPOIItem.MarkerType.RedPin);
+        markerSix.setMarkerType(MapPOIItem.MarkerType.CustomImage);
+        markerSix.setCustomImageResourceId(R.drawable.ic_marker);
+        markerSix.setCustomImageAutoscale(false);
         mapView.addPOIItem(markerSix);
 
         MapPOIItem markerSeven = new MapPOIItem();
         markerSeven.setMapPoint(MapPoint.mapPointWithGeoCoord(37.374591, 126.633828));
         markerSeven.setItemName("7 정보기술대학");
         markerSeven.setTag(7);
-        markerSeven.setMarkerType(MapPOIItem.MarkerType.BluePin);
-        markerSeven.setSelectedMarkerType(MapPOIItem.MarkerType.RedPin);
+        markerSeven.setMarkerType(MapPOIItem.MarkerType.CustomImage);
+        markerSeven.setCustomImageResourceId(R.drawable.ic_marker);
+        markerSeven.setCustomImageAutoscale(false);
         mapView.addPOIItem(markerSeven);
 
         MapPOIItem markerEight = new MapPOIItem();
         markerEight.setMapPoint(MapPoint.mapPointWithGeoCoord(37.373585, 126.632756));
         markerEight.setItemName("8 공과/도시과학대학");
         markerEight.setTag(8);
-        markerEight.setMarkerType(MapPOIItem.MarkerType.BluePin);
-        markerEight.setSelectedMarkerType(MapPOIItem.MarkerType.RedPin);
+        markerEight.setMarkerType(MapPOIItem.MarkerType.CustomImage);
+        markerEight.setCustomImageResourceId(R.drawable.ic_marker);
+        markerEight.setCustomImageAutoscale(false);
         mapView.addPOIItem(markerEight);
 
         MapPOIItem markerNine = new MapPOIItem();
         markerNine.setMapPoint(MapPoint.mapPointWithGeoCoord(37.372651, 126.633427));
         markerNine.setItemName("9 공동실험실습관");
         markerNine.setTag(9);
-        markerNine.setMarkerType(MapPOIItem.MarkerType.BluePin);
-        markerNine.setSelectedMarkerType(MapPOIItem.MarkerType.RedPin);
+        markerNine.setMarkerType(MapPOIItem.MarkerType.CustomImage);
+        markerNine.setCustomImageResourceId(R.drawable.ic_marker);
+        markerNine.setCustomImageAutoscale(false);
         mapView.addPOIItem(markerNine);
 
         MapPOIItem markerTen = new MapPOIItem();
         markerTen.setMapPoint(MapPoint.mapPointWithGeoCoord(37.372762, 126.631780));
         markerTen.setItemName("10 게스트하우스");
         markerTen.setTag(10);
-        markerTen.setMarkerType(MapPOIItem.MarkerType.BluePin);
-        markerTen.setSelectedMarkerType(MapPOIItem.MarkerType.RedPin);
+        markerTen.setMarkerType(MapPOIItem.MarkerType.CustomImage);
+        markerTen.setCustomImageResourceId(R.drawable.ic_marker);
+        markerTen.setCustomImageAutoscale(false);
         mapView.addPOIItem(markerTen);
 
         MapPOIItem markerEleven = new MapPOIItem();
         markerEleven.setMapPoint(MapPoint.mapPointWithGeoCoord(37.374476, 126.631785));
         markerEleven.setItemName("11 복지회관");
         markerEleven.setTag(11);
-        markerEleven.setMarkerType(MapPOIItem.MarkerType.BluePin);
-        markerEleven.setSelectedMarkerType(MapPOIItem.MarkerType.RedPin);
+        markerEleven.setMarkerType(MapPOIItem.MarkerType.CustomImage);
+        markerEleven.setCustomImageResourceId(R.drawable.ic_marker);
+        markerEleven.setCustomImageAutoscale(false);
         mapView.addPOIItem(markerEleven);
 
         MapPOIItem markerTwelve = new MapPOIItem();
         markerTwelve.setMapPoint(MapPoint.mapPointWithGeoCoord(37.375303, 126.632563));
         markerTwelve.setItemName("12 컨벤션센터");
         markerTwelve.setTag(12);
-        markerTwelve.setMarkerType(MapPOIItem.MarkerType.BluePin);
-        markerTwelve.setSelectedMarkerType(MapPOIItem.MarkerType.RedPin);
+        markerTwelve.setMarkerType(MapPOIItem.MarkerType.CustomImage);
+        markerTwelve.setCustomImageResourceId(R.drawable.ic_marker);
+        markerTwelve.setCustomImageAutoscale(false);
         mapView.addPOIItem(markerTwelve);
 
         MapPOIItem markerThirteen = new MapPOIItem();
         markerThirteen.setMapPoint(MapPoint.mapPointWithGeoCoord(37.376121, 126.633330));
         markerThirteen.setItemName("13 글로벌법정경대학");
         markerThirteen.setTag(13);
-        markerThirteen.setMarkerType(MapPOIItem.MarkerType.BluePin);
-        markerThirteen.setSelectedMarkerType(MapPOIItem.MarkerType.RedPin);
+        markerThirteen.setMarkerType(MapPOIItem.MarkerType.CustomImage);
+        markerThirteen.setCustomImageResourceId(R.drawable.ic_marker);
+        markerThirteen.setCustomImageAutoscale(false);
         mapView.addPOIItem(markerThirteen);
 
         MapPOIItem markerFourteen = new MapPOIItem();
-        markerFourteen.setMapPoint(MapPoint.mapPointWithGeoCoord(37.376776, 126.633016));
+        markerFourteen.setMapPoint(MapPoint.mapPointWithGeoCoord(37.376435, 126.632810));
         markerFourteen.setItemName("14 동북아국제통상/경영대학");
         markerFourteen.setTag(14);
-        markerFourteen.setMarkerType(MapPOIItem.MarkerType.BluePin);
-        markerFourteen.setSelectedMarkerType(MapPOIItem.MarkerType.RedPin);
+        markerFourteen.setMarkerType(MapPOIItem.MarkerType.CustomImage);
+        markerFourteen.setCustomImageResourceId(R.drawable.ic_marker);
+        markerFourteen.setCustomImageAutoscale(false);
         mapView.addPOIItem(markerFourteen);
 
         MapPOIItem markerFifteen = new MapPOIItem();
         markerFifteen.setMapPoint(MapPoint.mapPointWithGeoCoord(37.375651, 126.631997));
         markerFifteen.setItemName("15 인문대학");
         markerFifteen.setTag(15);
-        markerFifteen.setMarkerType(MapPOIItem.MarkerType.BluePin);
-        markerFifteen.setSelectedMarkerType(MapPOIItem.MarkerType.RedPin);
+        markerFifteen.setMarkerType(MapPOIItem.MarkerType.CustomImage);
+        markerFifteen.setCustomImageResourceId(R.drawable.ic_marker);
+        markerFifteen.setCustomImageAutoscale(false);
         mapView.addPOIItem(markerFifteen);
 
         MapPOIItem markerSixteen = new MapPOIItem();
-        markerSixteen.setMapPoint(MapPoint.mapPointWithGeoCoord(37.376121, 126.633330));
+        markerSixteen.setMapPoint(MapPoint.mapPointWithGeoCoord(37.374732, 126.631260));
         markerSixteen.setItemName("16 예술체육대학");
         markerSixteen.setTag(16);
-        markerSixteen.setMarkerType(MapPOIItem.MarkerType.BluePin);
-        markerSixteen.setSelectedMarkerType(MapPOIItem.MarkerType.RedPin);
+        markerSixteen.setMarkerType(MapPOIItem.MarkerType.CustomImage);
+        markerSixteen.setCustomImageResourceId(R.drawable.ic_marker);
+        markerSixteen.setCustomImageAutoscale(false);
         mapView.addPOIItem(markerSixteen);
 
         MapPOIItem markerSeventeen = new MapPOIItem();
-        markerSeventeen.setMapPoint(MapPoint.mapPointWithGeoCoord(37.376121, 126.633330));
+        markerSeventeen.setMapPoint(MapPoint.mapPointWithGeoCoord(37.374213, 126.630748));
         markerSeventeen.setItemName("17 학생회관");
         markerSeventeen.setTag(17);
-        markerSeventeen.setMarkerType(MapPOIItem.MarkerType.BluePin);
-        markerSeventeen.setSelectedMarkerType(MapPOIItem.MarkerType.RedPin);
+        markerSeventeen.setMarkerType(MapPOIItem.MarkerType.CustomImage);
+        markerSeventeen.setCustomImageResourceId(R.drawable.ic_marker);
+        markerSeventeen.setCustomImageAutoscale(false);
         mapView.addPOIItem(markerSeventeen);
 
         MapPOIItem markerEighteen = new MapPOIItem();
         markerEighteen.setMapPoint(MapPoint.mapPointWithGeoCoord(37.373849, 126.629789));
         markerEighteen.setItemName("18 생활원");
         markerEighteen.setTag(18);
-        markerEighteen.setMarkerType(MapPOIItem.MarkerType.BluePin);
-        markerEighteen.setSelectedMarkerType(MapPOIItem.MarkerType.RedPin);
+        markerEighteen.setMarkerType(MapPOIItem.MarkerType.CustomImage);
+        markerEighteen.setCustomImageResourceId(R.drawable.ic_marker);
+        markerEighteen.setCustomImageAutoscale(false);
         mapView.addPOIItem(markerEighteen);
 
         MapPOIItem markerNineteen = new MapPOIItem();
         markerNineteen.setMapPoint(MapPoint.mapPointWithGeoCoord(37.374586, 126.630250));
         markerNineteen.setItemName("19 국제교류관");
         markerNineteen.setTag(19);
-        markerNineteen.setMarkerType(MapPOIItem.MarkerType.BluePin);
-        markerNineteen.setSelectedMarkerType(MapPOIItem.MarkerType.RedPin);
+        markerNineteen.setMarkerType(MapPOIItem.MarkerType.CustomImage);
+        markerNineteen.setCustomImageResourceId(R.drawable.ic_marker);
+        markerNineteen.setCustomImageAutoscale(false);
         mapView.addPOIItem(markerNineteen);
 
         MapPOIItem markerTwenty = new MapPOIItem();
         markerTwenty.setMapPoint(MapPoint.mapPointWithGeoCoord(37.374893, 126.629708));
         markerTwenty.setItemName("20 스포츠센터");
         markerTwenty.setTag(20);
-        markerTwenty.setMarkerType(MapPOIItem.MarkerType.BluePin);
-        markerTwenty.setSelectedMarkerType(MapPOIItem.MarkerType.RedPin);
+        markerTwenty.setMarkerType(MapPOIItem.MarkerType.CustomImage);
+        markerTwenty.setCustomImageResourceId(R.drawable.ic_marker);
+        markerTwenty.setCustomImageAutoscale(false);
         mapView.addPOIItem(markerTwenty);
 
         MapPOIItem markerTwentyOne = new MapPOIItem();
         markerTwentyOne.setMapPoint(MapPoint.mapPointWithGeoCoord(37.375520, 126.630261));
         markerTwentyOne.setItemName("21 체육관");
         markerTwentyOne.setTag(21);
-        markerTwentyOne.setMarkerType(MapPOIItem.MarkerType.BluePin);
-        markerTwentyOne.setSelectedMarkerType(MapPOIItem.MarkerType.RedPin);
+        markerTwentyOne.setMarkerType(MapPOIItem.MarkerType.CustomImage);
+        markerTwentyOne.setCustomImageResourceId(R.drawable.ic_marker);
+        markerTwentyOne.setCustomImageAutoscale(false);
         mapView.addPOIItem(markerTwentyOne);
 
         MapPOIItem markerTwentyTwo = new MapPOIItem();
         markerTwentyTwo.setMapPoint(MapPoint.mapPointWithGeoCoord(37.375870, 126.630819));
         markerTwentyTwo.setItemName("22 학군단");
         markerTwentyTwo.setTag(22);
-        markerTwentyTwo.setMarkerType(MapPOIItem.MarkerType.BluePin);
-        markerTwentyTwo.setSelectedMarkerType(MapPOIItem.MarkerType.RedPin);
+        markerTwentyTwo.setMarkerType(MapPOIItem.MarkerType.CustomImage);
+        markerTwentyTwo.setCustomImageResourceId(R.drawable.ic_marker);
+        markerTwentyTwo.setCustomImageAutoscale(false);
         mapView.addPOIItem(markerTwentyTwo);
 
         MapPOIItem markerTwentyThree = new MapPOIItem();
         markerTwentyThree.setMapPoint(MapPoint.mapPointWithGeoCoord(37.377856, 126.632536));
         markerTwentyThree.setItemName("23 강당 및 공연장");
         markerTwentyThree.setTag(23);
-        markerTwentyThree.setMarkerType(MapPOIItem.MarkerType.BluePin);
-        markerTwentyThree.setSelectedMarkerType(MapPOIItem.MarkerType.RedPin);
+        markerTwentyThree.setMarkerType(MapPOIItem.MarkerType.CustomImage);
+        markerTwentyThree.setCustomImageResourceId(R.drawable.ic_marker);
+        markerTwentyThree.setCustomImageAutoscale(false);
         mapView.addPOIItem(markerTwentyThree);
 
         MapPOIItem markerTwentyFour = new MapPOIItem();
         markerTwentyFour.setMapPoint(MapPoint.mapPointWithGeoCoord(37.375912, 126.635805));
         markerTwentyFour.setItemName("24 전망타워");
         markerTwentyFour.setTag(24);
-        markerTwentyFour.setMarkerType(MapPOIItem.MarkerType.BluePin);
-        markerTwentyFour.setSelectedMarkerType(MapPOIItem.MarkerType.RedPin);
+        markerTwentyFour.setMarkerType(MapPOIItem.MarkerType.CustomImage);
+        markerTwentyFour.setCustomImageResourceId(R.drawable.ic_marker);
+        markerTwentyFour.setCustomImageAutoscale(false);
         mapView.addPOIItem(markerTwentyFour);
 
         MapPOIItem markerTwentyFive = new MapPOIItem();
         markerTwentyFive.setMapPoint(MapPoint.mapPointWithGeoCoord(37.375226, 126.636014));
         markerTwentyFive.setItemName("25 어린이집");
         markerTwentyFive.setTag(25);
-        markerTwentyFive.setMarkerType(MapPOIItem.MarkerType.BluePin);
-        markerTwentyFive.setSelectedMarkerType(MapPOIItem.MarkerType.RedPin);
+        markerTwentyFive.setMarkerType(MapPOIItem.MarkerType.CustomImage);
+        markerTwentyFive.setCustomImageResourceId(R.drawable.ic_marker);
+        markerTwentyFive.setCustomImageAutoscale(false);
         mapView.addPOIItem(markerTwentyFive);
 
         MapPOIItem markerTwentySix = new MapPOIItem();
         markerTwentySix.setMapPoint(MapPoint.mapPointWithGeoCoord(37.375205, 126.635574));
         markerTwentySix.setItemName("26 온실");
         markerTwentySix.setTag(26);
-        markerTwentySix.setMarkerType(MapPOIItem.MarkerType.BluePin);
-        markerTwentySix.setSelectedMarkerType(MapPOIItem.MarkerType.RedPin);
+        markerTwentySix.setMarkerType(MapPOIItem.MarkerType.CustomImage);
+        markerTwentySix.setCustomImageResourceId(R.drawable.ic_marker);
+        markerTwentySix.setCustomImageAutoscale(false);
         mapView.addPOIItem(markerTwentySix);
 
         MapPOIItem markerTwentySeven = new MapPOIItem();
-        markerTwentySeven.setMapPoint(MapPoint.mapPointWithGeoCoord(37.371958, 126.633177));
+        markerTwentySeven.setMapPoint(MapPoint.mapPointWithGeoCoord(37.372260, 126.633069));
         markerTwentySeven.setItemName("27 제2공동실험실습관");
         markerTwentySeven.setTag(27);
-        markerTwentySeven.setMarkerType(MapPOIItem.MarkerType.BluePin);
-        markerTwentySeven.setSelectedMarkerType(MapPOIItem.MarkerType.RedPin);
+        markerTwentySeven.setMarkerType(MapPOIItem.MarkerType.CustomImage);
+        markerTwentySeven.setCustomImageResourceId(R.drawable.ic_marker);
+        markerTwentySeven.setCustomImageAutoscale(false);
         mapView.addPOIItem(markerTwentySeven);
 
         MapPOIItem markerTwentyEight = new MapPOIItem();
-        markerTwentyEight.setMapPoint(MapPoint.mapPointWithGeoCoord(37.371758, 126.633016));
+        markerTwentyEight.setMapPoint(MapPoint.mapPointWithGeoCoord(37.371896, 126.632616));
         markerTwentyEight.setItemName("28 도시과학대학");
         markerTwentyEight.setTag(28);
-        markerTwentyEight.setMarkerType(MapPOIItem.MarkerType.BluePin);
-        markerTwentyEight.setSelectedMarkerType(MapPOIItem.MarkerType.RedPin);
+        markerTwentyEight.setMarkerType(MapPOIItem.MarkerType.CustomImage);
+        markerTwentyEight.setCustomImageResourceId(R.drawable.ic_marker);
+        markerTwentyEight.setCustomImageAutoscale(false);
         mapView.addPOIItem(markerTwentyEight);
 
         MapPOIItem markerTwentyNine = new MapPOIItem();
-        markerTwentyNine.setMapPoint(MapPoint.mapPointWithGeoCoord(37.371758, 126.633016));
+        markerTwentyNine.setMapPoint(MapPoint.mapPointWithGeoCoord(37.372468, 126.631209));
         markerTwentyNine.setItemName("29 생명공학부");
         markerTwentyNine.setTag(29);
-        markerTwentyNine.setMarkerType(MapPOIItem.MarkerType.BluePin);
-        markerTwentyNine.setSelectedMarkerType(MapPOIItem.MarkerType.RedPin);
+        markerTwentyNine.setMarkerType(MapPOIItem.MarkerType.CustomImage);
+        markerTwentyNine.setCustomImageResourceId(R.drawable.ic_marker);
+        markerTwentyNine.setCustomImageAutoscale(false);
         mapView.addPOIItem(markerTwentyNine);
 
         MapPOIItem markerThirty = new MapPOIItem();
         markerThirty.setMapPoint(MapPoint.mapPointWithGeoCoord(37.374367, 126.629185));
         markerThirty.setItemName("30 제2기숙사");
         markerThirty.setTag(30);
-        markerThirty.setMarkerType(MapPOIItem.MarkerType.BluePin);
-        markerThirty.setSelectedMarkerType(MapPOIItem.MarkerType.RedPin);
+        markerThirty.setMarkerType(MapPOIItem.MarkerType.CustomImage);
+        markerThirty.setCustomImageResourceId(R.drawable.ic_marker);
+        markerThirty.setCustomImageAutoscale(false);
         mapView.addPOIItem(markerThirty);
     }
 
